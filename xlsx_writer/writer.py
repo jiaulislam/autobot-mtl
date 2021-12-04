55cd7a788ef1c5ffcdfb12f06160df287a9d22c4
jiaulislam <jiaulislam.ict.bd@gmail.com>
2021-12-04 13:09:45 +0600

    done

--- xlsx_writer/writer.py
+++ xlsx_writer/writer.py
@@ -3,11 +3,10 @@ from xlsxwriter import Workbook
 from xlsxwriter.worksheet import Worksheet
 
 
-class SheetHeader:
+class SheetWrapper:
 
-    @staticmethod
-    def get_header() -> list[str]:
-        return ['SL NO',
+    headers: List[str] = [
+                'SL NO',
                 'Date',
                 'Project Coordinator',
                 'Project Name',
@@ -18,12 +17,13 @@ class SheetHeader:
                 'Commercial Zone',
                 'NCR Number',
                 'Change Manager',
-                'Current Status']
+                'Current Status'
+    ]
 
 
 
 class Writer:
-    ___db_data_length = 1
+    __db_data_length = 1
 
     def __init__(self, file_name: str, sheet_name: str='Main'):
         self.file_name = file_name
@@ -38,7 +38,7 @@ class Writer:
         self.close()
 
     def write_to_excel(self, _db_data: List[Tuple]) -> None:
-        self.___db_data_length += self.get_data_length(_db_data)
+        self.__db_data_length += self.get_data_length(_db_data)
 
         for _row in range(1,len(_db_data)+1):
             for _idx, _col in enumerate(_db_data[_row-1]):
@@ -47,7 +47,7 @@ class Writer:
                 elif _idx == 1:
                     self.worksheet.write_datetime(_row, _idx, _col, self.__date_format)
                 elif _idx == 12:
-                    # * no need to give user to create_at value
+                    # no need to give user created_at value
                     pass
                 else:
                     self.worksheet.write_string(_row, _idx, str(_col))
@@ -61,9 +61,9 @@ class Writer:
     def __make_table(self):
         _options = {
             'style': 'Table Style Light 17',
-            'columns': [{'header': i.upper()} for i in SheetHeader.get_header()]
+            'columns': [{'header': i.upper()} for i in SheetWrapper.headers]
         }
-        self.worksheet.add_table(f'A1:L{self.___db_data_length}', options=_options)
+        self.worksheet.add_table(f'A1:L{self.__db_data_length}', options=_options)
 
     def close(self):
         self.workbook.close()
